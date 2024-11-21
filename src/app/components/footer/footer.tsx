e5bdb54d3ff720439efbd064d82673944bdc952d
Brian Giusti <briangiusti@Brians-MacBook-Pro-2.local>
2024-11-21 16:39:21 -0500

    Add Header and footer to Home Page

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <footer>
+      <p>Brian Giusti 2024</p>
+    </footer>
+    );
+  }
---
+++ src/app/components/mainContent/homePage.tsx
@@ -0,0 +1,9 @@
+export default function MainContent() {
+  return (
+    <>
+      <h1>Brian Giusti</h1>
+      <h2>Software Engineer & Aspiring Product Manager</h2>
+      <p>Creating product solutions customers love</p>
+    </>
+  );
+}
