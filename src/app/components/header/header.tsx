64d049b07f771dcb0b68e93da76804626951b606
Brian Giusti <briangiusti@Brians-MacBook-Pro-2.local>
2024-11-21 16:45:05 -0500

    Fix links

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
       <header className="header-wrapper">
       <nav className="header-nav-wrapper">
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/projects">Projects</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
         </ul>
       </nav>
     </header>
